79331ec75176f567ee1255f74e17269d3cae98f4
AnanthaNagaraj <arohit695@gmail.com>
2021-06-04 13:13:44 +0530

    pushed code to branch

---
+++ src/com/test/Sample9_GitExample.java
@@ -0,0 +1,38 @@
+package com.test;
+
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class Sample9_GitExample extends Base{
+
+	//@Parameters({"URL"})
+	@Test
+	public void test1()
+	{
+		
+		System.out.println("Sample 7 ---------> Test 1");
+	}
+	
+	//@Parameters({"URL"})
+	@Test
+	public void test2()
+	{
+		
+		System.out.println("Sample 7 ---------> Test 2");
+	}
+	
+	//==========
+	@Test
+	public void test22()
+	{
+		
+		System.out.println("Sample 7 ---------> Test 2");
+	}
+	
+	@Test
+	public void test21()
+	{
+		
+		System.out.println("Sample 7 ---------> Test 2");
+	}
+}
