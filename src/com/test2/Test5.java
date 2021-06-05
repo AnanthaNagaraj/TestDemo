286423c31fff289421f9c93742bd9c1051bbca31
AnanthaNagaraj <arohit695@gmail.com>
2021-06-05 20:46:31 +0530

    addition of two number

---
+++ src/com/test2/Test5.java
@@ -0,0 +1,20 @@
+package com.test2;
+
+import org.testng.annotations.Test;
+
+
+public class Test5 {
+
+	@Test	
+	public void test()
+	{
+		System.out.println("Hello");
+		int a =3;
+		int b=5;
+		int c = a+b;
+		System.out.println("Add : " +c);
+	}
+	
+	
+	
+}
