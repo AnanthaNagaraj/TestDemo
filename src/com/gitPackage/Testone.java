c571d6008b0f9421340b11ff3dca0147ee0b020a
ChaithanyaYK <chaithu.82009@gmail.com>
2021-06-17 20:11:58 +0530

    hello

--- src/com/gitPackage/Testone.java
+++ src/com/gitPackage/Testone.java
@@ -4,9 +4,9 @@ public class Testone {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hi");
-// git commit at gitPackage
-System.out.println("Hello");
+		System.out.println("hi");
+		// git commit at gitPackage
+		System.out.println("Hello");
 	}
 
 }
